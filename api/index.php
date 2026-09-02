d8cc52f733a04fd0732712d4c3ef7f1fafd420b3
mezzaluna0723-arch <mezzaluna0723@gmail.com>
2026-09-02 22:08:33 +0700

    Update api/index.php handler

--- api/index.php
+++ api/index.php
@@ -1,3 +1,20 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Register the Auto Loader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Turn On The Lights
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Run The Application
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
