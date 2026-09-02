0a90bc22498e5789fdf317de64dea4a089aa17b3
mezzaluna0723-arch <mezzaluna0723@gmail.com>
2026-09-02 22:24:09 +0700

    Fix Laravel storage cache path for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
+// Forward request to the public/index.php
+// Force storage and bootstrap cache to Vercel /tmp directory
+$tmpDir = '/tmp';
+putenv("APP_SERVICES_CACHE={$tmpDir}/services.php");
+putenv("APP_PACKAGES_CACHE={$tmpDir}/packages.php");
+putenv("APP_CONFIG_CACHE={$tmpDir}/config.php");
+putenv("APP_ROUTES_CACHE={$tmpDir}/routes.php");
+
 require __DIR__ . '/../public/index.php';
