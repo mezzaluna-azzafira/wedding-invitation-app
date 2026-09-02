6b14f97fd116cb55d8b2108f74693a4daf68ab01
mezzaluna0723-arch <mezzaluna0723@gmail.com>
2026-09-02 22:14:11 +0700

    Fix vercel runtime config for PHP

--- api/index.php
+++ api/index.php
@@ -1,20 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Register the Auto Loader
-require __DIR__ . '/../vendor/autoload.php';
-
-// Turn On The Lights
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Run The Application
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
