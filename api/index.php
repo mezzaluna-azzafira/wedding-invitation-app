e5ccb1f6bcfbf0790922df58087a338c0b14fd33
mezzaluna0723-arch <mezzaluna0723@gmail.com>
2026-09-03 11:44:29 +0700

    fix ssl verfication

--- api/index.php
+++ api/index.php
@@ -1,11 +1,4 @@
 <?php
 
-// Forward request to the public/index.php
-// Force storage and bootstrap cache to Vercel /tmp directory
-$tmpDir = '/tmp';
-putenv("APP_SERVICES_CACHE={$tmpDir}/services.php");
-putenv("APP_PACKAGES_CACHE={$tmpDir}/packages.php");
-putenv("APP_CONFIG_CACHE={$tmpDir}/config.php");
-putenv("APP_ROUTES_CACHE={$tmpDir}/routes.php");
-
+// Forward request to Laravel's public entrypoint
 require __DIR__ . '/../public/index.php';
--- config/database.php
+++ config/database.php
@@ -60,9 +60,10 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
-
+        
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
