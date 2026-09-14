94b31a16da14159821a26d36f843be9b1fe01786
mezzaluna-azzafira <mezza23lunaazzafira@gmail.com>
2026-09-14 12:48:42 +0700

    fix: hardcode default log channel to stderr

--- config/logging.php
+++ config/logging.php
@@ -7,7 +7,7 @@ use Monolog\Processor\PsrLogMessageProcessor;
 
 return [
 
-    'default' => env('LOG_CHANNEL') ?: 'stderr',
+    'default' => 'stderr',
 
     'deprecations' => [
         'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
