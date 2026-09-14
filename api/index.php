d2ecdb3cd09072234eb396299e5c241a9adf947d
mezzaluna-azzafira <mezza23lunaazzafira@gmail.com>
2026-09-14 16:10:26 +0700

    fix: kembalikan api index ke versi sukses 200

--- api/index.php
+++ api/index.php
@@ -1,17 +1,3 @@
 <?php
 
-// 1. Panggil autoloader dari Composer (Ini yang tadi hilang!)
-require __DIR__ . '/../vendor/autoload.php';
-
-// 2. Jalankan aplikasi Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
