1b29d6cf20ac64d5bedd672700349bd0a51bb6aa
mezzaluna-azzafira <mezza23lunaazzafira@gmail.com>
2026-09-14 13:05:37 +0700

    force override log channel in api index

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
 
+// Paksa log driver ke stderr secara instan sebelum Laravel loading
+$_ENV['LOG_CHANNEL'] = 'stderr';
+putenv('LOG_CHANNEL=stderr');
+
 require __DIR__ . '/../public/index.php';
