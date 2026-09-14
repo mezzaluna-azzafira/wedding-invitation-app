71ad75200461193fc56c4141bb74ca2860442c46
mezzaluna-azzafira <mezza23lunaazzafira@gmail.com>
2026-09-14 16:01:45 +0700

    fix: update api index handler for vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-// Forward request ke public/index.php bawaan Laravel
-require __DIR__ . '/../public/index.php';
+// Forward permintaan Vercel ke public/index.php bawaan Laravel
+define('LARAVEL_START', microtime(true));
+
+// Mengarahkan folder storage dan cache ke /tmp (karena Vercel read-only)
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
