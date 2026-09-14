2168b5ceafb79cc7ce16c2d5c3e092df6559f915
mezzaluna-azzafira <mezza23lunaazzafira@gmail.com>
2026-09-14 14:38:47 +0700

    Update api/index.php for Vercel logging and error handling

--- api/index.php
+++ api/index.php
@@ -1,8 +1,13 @@
 <?php
 
-// Set variabel environment langsung ke 'stderr'
+// 1. Sembunyikan error dari layar pengunjung (Keamanan)
+ini_set('display_errors', '0');
+error_reporting(E_ALL);
+
+// 2. Alirkan log error langsung ke Dashboard Vercel
 putenv('LOG_CHANNEL=stderr');
 $_ENV['LOG_CHANNEL'] = 'stderr';
-$_SERVER['LOG_CHANNEL'] = 'stderr';
+$_SERVER['LOG_SERVER'] = 'stderr';
 
+// 3. Panggil file index Laravel utama
 require __DIR__ . '/../public/index.php';
